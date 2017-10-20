29b1cb2059e744d49b949c25a30e5a34fa2ae34c
zaigui9lala <1005089574@qq.com>
2017-10-20 09:06:46 +0800

    init

--- app/src/main/java/com/android/robotmap/MainActivity.java
+++ app/src/main/java/com/android/robotmap/MainActivity.java
@@ -331,8 +331,6 @@ public class MainActivity extends FragmentActivity implements
     @Override
     public void onGeoFenceCreateFinished(List<GeoFence> list, int i, String s) {
 
-
-
     }
 
     private class HandlerThread implements Runnable {
