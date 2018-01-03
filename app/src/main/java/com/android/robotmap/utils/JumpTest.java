3228b3960e4cae7f1a2e5b7cdad8e2da6f3badea
zaigui9lala <1005089574@qq.com>
2018-01-03 17:13:41 +0800

    init

---
+++ app/src/main/java/com/android/robotmap/utils/JumpTest.java
@@ -0,0 +1,22 @@
+package com.android.robotmap.utils;
+
+/**
+ * Created by Administrator on 2018/1/2.
+ */
+
+public class JumpTest {
+    public static void main(String[] args) {
+        calc(117, 57);
+    }
+
+    private static void calc(double v, double v2) {
+        double v3 = hypotenuse(v, v2);
+        System.out.println((v3 / 145.619367) * 769 + "");
+    }
+
+    public static double hypotenuse(double a, double b) {
+
+        return Math.sqrt(a * a + b * b);
+
+    }
+}
